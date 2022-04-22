784e0ba23334e1c7b0afaa50d72b67cac38575ab
pung <fp94214@gmail.com>
2022-04-22 19:08:56 +0800

    ~

--- packages/build/src/paths.ts
+++
@@ -1,5 +0,0 @@
-import { resolve } from 'path'
-
-export const projRoot = resolve(__dirname, '..', '..', '..')
-
-export const buildRoot = resolve(projRoot, 'packages', 'build')
--- play/src/pages/button.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <en-button type="primary">按钮</en-button>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
