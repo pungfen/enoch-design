4df334e1056b5b4815d5aec15120a275ce1acdaf
pung <fp94214@live.com>
2022-05-09 01:46:45 +0800

    ~

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
-//@ts-ignore
 import { setupLayouts } from 'virtual:generated-layouts'
 
 import App from './app.vue'
 
-//@ts-ignore
 import routes from '~pages'
+
+// import EnochComponents from '@enoch/components'
+
+// console.log(EnochComponents)
 
 export const router = createRouter({
   routes: setupLayouts(routes),
