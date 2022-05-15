632ed03f897c7797a6b585895e9ac0a10deb279b
pung <fp94214@live.com>
2022-05-16 03:00:29 +0800

    ~

--- packages/hooks/src/use-factory.ts
+++ packages/hooks/src/use-factory.ts
@@ -5,20 +5,9 @@ import type { Any, Object } from 'ts-toolbelt'
 import type { Method } from 'axios'
 
 type ComponentType = 'table' | 'form' | 'tree' | 'dialog' | 'drawer'
+type KeyWord = 'type' | 'ajax' | 'data'
 
-enum Component {
-  Table = 1,
-  Form = 2
-}
-
-interface TableConfig extends AjaxConfig {
-  type?: 'table'
-  data?: unknown[]
-}
-
-interface FormConfig extends AjaxConfig {
-  initializer?: () => Record<string, any>
-}
+type TablePaging = { current: number; size: number }
 
 type AjaxActionConfig = {
   action: `${Method} /${string}`
@@ -32,33 +21,128 @@ type AjaxActionConfig = {
   }
 }
 
+interface IndexConfig {
+  type?: ComponentType
+  ajax?: Record<string, AjaxActionConfig>
+  data?: any
+}
+
+interface TableConfig {
+  data: unknown[]
+  paging?: TablePaging
+}
+
+interface TableReturn {
+  data: unknown[]
+  paging: TablePaging
+}
+
+interface FormConfig {
+  data?: Record<string, any>
+  initializer?: () => Record<string, any>
+}
+
+interface FormReturn {
+  data: object
+}
+
 interface FactoryBaseState {
   $emit?: SetupContext['emit']
   $slots?: SetupContext['slots']
   [index: string]: any
 }
 
-interface AjaxConfig {
-  ajax?: AjaxActionConfig
+// type FactoryThisType<Config> = {
+//   [Key in keyof Config]: Config[Key] extends infer SubConfig
+//     ? SubConfig extends Record<string, any>
+//       ? Object.Has<SubConfig, 'type', ComponentType, '<-extends'> extends 1
+//         ? Object.Merge<
+//             MutableRecord<{
+//               ['table']: TableReturn
+//               ['form']: FormReturn
+//             }>,
+//             FactoryThisType<Object.Omit<SubConfig, KeyWord>>
+//           >
+//         : SubConfig
+//       : SubConfig
+//     : Config[Key]
+// }
+
+// type convertComponentConfig<Config extends object = object> = {
+//   [Key in keyof Config]: Config[Key] extends object
+//     ? Object.Has<Config[Key], 'type', ComponentType, '<-extends'> extends 1
+//       ? MutableRecord<{
+//           ['table']: TableConfig
+//           ['form']: FormConfig
+//         }>
+//       : never
+//     : never
+// }
+
+type OmitNever<T> = Pick<T, { [P in keyof T]: T[P] extends never ? never : P }[keyof T]>
+
+type ConvertIndexConfig<Config extends object = object> = {
+  [Key in keyof Config]: { name: string }
+}[keyof Config]
+
+let testIndex: FactoryConfig = {
+  type: { name: '' },
+  data: {},
+  obj: {
+    name: ''
+  }
 }
 
-type test2 = Any.Contains<{ a: string }, { a: string; b: number }> // False
-type test3 = Object.Has<{ type: 'table'; name: 'xxx' }, 'type', 'table', 'equals'>
-
-// type IsComponent<Config> = 'type' extends keyof Config ? (Config['type'] extends ComponentType ? Config : never) : never
-type FactoryIndexConfig<Config extends Record<string, any>> = Object.Has<Config, 'type', 'table', 'equals'> extends 0
-  ? TableConfig
-  : Object.Has<Config, 'type', 'form', 'equals'> extends 0
-  ? FormConfig
+type ConvertTypeConfig<Config extends object> = 'type' extends keyof Config
+  ? 'table' extends Config['type']
+    ? TableConfig & ConvertIndexConfig<Omit<Config, keyof TableConfig>>
+    : never
   : never
 
-type FactoryConfig<Config extends Record<string, any> = Record<string, any>> = {
-  [Key in keyof Config]: Config[Key] extends Record<string, any> ? FactoryIndexConfig<Config[Key]> : Config[Key]
-}
+// ? 'table' extends SubConfig['type']
+//           ? IndexConfig & TableConfig & FactoryConfig<Omit<SubConfig, 'type' | 'ajax'>>
+//           : 'form' extends SubConfig['type']
+//           ? IndexConfig & FormConfig & FactoryConfig<Omit<SubConfig, 'type' | 'ajax'>>
+//           : never
+//ThisType<Object.Merge<FactoryThisType<Config>, FactoryBaseState>>
 
-type UseFactoryReturn = UnwrapRef<FactoryBaseState>
+type FactoryConfig<Config extends object = object> = ConvertIndexConfig<Config>
 
-export const useFactory = <Config extends Record<string, any>>(config: FactoryConfig<Config>): UseFactoryReturn => {
+// keyof Omit<SubConfig, 'type' | 'ajax'>]?: SubConfig[SubKey] extends object
+// ? { name: string }
+// : SubConfig[SubKey]
+// }
+
+// Config[Key] extends infer SubConfig
+//     ? Any.Equals<SubConfig, object> extends 1
+//       ? IndexConfig
+//       : SubConfig
+//     : never
+
+// ? Object.Has<SubConfig, 'type', ComponentType, '<-extends'> extends 1
+//         ? Object.Merge<
+//             MutableRecord<{
+//               ['table']: TableConfig
+//               ['form']: FormConfig
+//             }>,
+//             // [
+//             //   { ajax: AjaxConfig },
+//             //   {
+//             //     [SubKey in keyof Object.Omit<Config[Key], KeyWord>]: Config[Key] extends infer SubConfig
+//             //       ? SubConfig extends object
+//             //         ? FactoryConfig<SubConfig>
+//             //         : SubConfig
+//             //       : never
+//             //   }
+//             // ]
+//             {}
+//           >
+//         : FactoryConfig<SubConfig>
+//       : SubConfig
+
+type UseFactoryReturn<Config> = UnwrapRef<FactoryBaseState>
+
+export const useFactory = <Config extends object>(config: FactoryConfig<Config>): UseFactoryReturn<Config> => {
   const vm = getCurrentInstance()
   const slots = useSlots()
 
@@ -70,12 +154,20 @@ export const useFactory = <Config extends Record<string, any>>(config: FactoryCo
 }
 
 const xx = useFactory({
-  name: '',
-  table: {
+  name: '2',
+  obj: {
     type: 'table',
-    data: []
-  },
-  form: {
-    type: 'form'
+    data: [],
+    ajax: {
+      get: {
+        action: 'GET /xx'
+      }
+    },
+    xxx: {
+      type: 'form',
+      data: {}
+    }
   }
 })
+
+type A = Any.Equals<Record<string, any>, Record<string, any>>
