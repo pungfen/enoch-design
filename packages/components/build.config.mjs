c4c67ef68d25c3c9398d09719cbecc828bf7ae2a
pung <fp94214@live.com>
2022-06-08 01:01:23 +0800

    ~

---
+++ packages/components/build.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  name: '@enochfe/components',
+  build: {
+    srcDir: 'src',
+    namedExport: true,
+    packageManager: 'pnpm',
+    extensions: {
+      esm: '.mjs'
+    }
+  }
+}
