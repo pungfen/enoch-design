daabf12d26c846feaaf53817778094dc04c96a64
pung <fp94214@gmail.com>
2022-09-28 11:27:33 +0800

    ~

--- packages/build/tsup.config.ts
+++ packages/build/tsup.config.ts
@@ -2,7 +2,5 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   clean: true,
-  dts: true,
-  entry: ['src/cli.ts', 'src/index.ts'],
-  format: ['esm']
+  entry: ['src/cli.ts', 'src/index.ts']
 })
