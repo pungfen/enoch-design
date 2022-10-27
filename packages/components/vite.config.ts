ae268e0a4a1807321625e2c8b8a010296d21d2a3
pung <fp94214@gmail.com>
2022-10-27 18:33:07 +0800

    xx

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -6,6 +6,7 @@ import dts from 'vite-plugin-dts'
 import unocss from 'unocss/vite'
 import { presetUno, presetAttributify, presetIcons } from 'unocss'
 import transformerDirective from '@unocss/transformer-directives'
+import colors from 'tailwindcss/colors'
 
 export default defineConfig({
   plugins: [
@@ -19,7 +20,21 @@ export default defineConfig({
       transformers: [transformerDirective()],
       theme: {
         colors: {
-          primary: '#4C58D9'
+          ...colors,
+          primary: {
+            900: '#4C58D9'
+          }
+        },
+        fontSize: {
+          xs: '0.75rem',
+          sm: '0.875rem',
+          base: '1rem',
+          lg: '1.125rem',
+          xl: '1.25rem',
+          '2xl': '1.5rem',
+          '3xl': '1.875rem',
+          '4xl': '2.25rem',
+          '5xl': '3rem'
         }
       }
     })
